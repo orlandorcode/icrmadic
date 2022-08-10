c504e0544a0835bb135e86973965e2d2aa86ffd5
orlandorcode <101308252+orlandorcode@users.noreply.github.com>
2022-08-09 22:54:00 -0500

    Update twtimeline.py
    Versión 0.02 
    Toma los datos, los ingresa a una base de datos de pandas y limpia los datos, de manera que reconoce cadenas de texto, números y fecha, facilitando el procesamiento posterior. Ahí vamos!

--- twtimeline.py
+++ twtimeline.py
@@ -12,14 +12,15 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-df = pd.DataFrame(columns = ['id', 'nombre', 'texto','mencion', 'fecha'])
-
+df = pd.DataFrame(columns = ['id', 'nombre', 'texto','mencion', 'retweets', 'favoritos', 'fecha'])
 
 while True:
-    acct = input('Enter a Twitter account, or quit: ')
-    if (acct == 'quit'): break
+    acct = input('Ingresar una cuenta de Twitter o salir: ')
+    if (acct == 'salir'): break
 
-    url = twurl.augment(TWITTER_URL, {'screen_name': acct, 'count': '100'})
+    # count indica la cantidad de tweets a capturar
+
+    url = twurl.augment(TWITTER_URL, {'screen_name': acct, 'count': '5'})
     print('Retrieving', url)
     connection = urlopen(url, context=ctx)
     data = connection.read().decode()
@@ -29,28 +30,48 @@ while True:
     js = json.loads(data)
 
 
-    # Debugging
-    # print(json.dumps(js, indent=4))
+    # Para imprimir los datos tal cual los entrega Twitter
+    # print(json.dumps(js, indent=4))
 
     # Itera a través de la lista de diccionarios de JSON
     # y captura los datos que buscamos
 
     for u in js:
         info = ({
-            'id' : [u['id']],
-            'nombre' : [u["user"]["screen_name"]],
-            'texto' : [u["text"]],
-            'fecha' : [u["created_at"]]
+            'id' : u['id'],
+            'nombre' : u["user"]["screen_name"],
+            'texto' : u["text"],
+            'retweets': u["retweet_count"],
+            'favoritos': u["favorite_count"],
+            'fecha' : u["created_at"]
         })
         try :
             mencion = u["entities"]["user_mentions"][0]["screen_name"],
-            info['mencion'] = [mencion]
+            info['mencion'] = mencion
         except:
-            info['mencion'] = ["Sin menciones"]
+            info['mencion'] = "Sin menciones"
             # Debugging
             #print(info)
         info = pd.Series(info)
         df = df.append(info, ignore_index=True)
 
 
+# Preparación de datos
+# Diccionario de conversion
+
+conversion_dict  =  {'id' : int,
+                    'retweets' : int,
+                    'favoritos' : int,
+                    }
+
+df = df.astype(conversion_dict)
+
+# Termina de convertir otros que no pude convertir con el diccionario.
+
+df['fecha'] = pd.to_datetime(df['fecha'])
+df['nombre'] = df['nombre'].astype('string')
+df['mencion'] = df['mencion'].astype('string')
+df['texto'] = df['texto'].astype('string')
+
+# Para exportar la base de datos en el archivo tweets.csv
 df.to_csv('tweets.csv', index=False)
